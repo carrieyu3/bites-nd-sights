e2bdb4436ac53e7be9682a50bb961139ca16cfe6
Michael Paredes <michael.mp915@gmail.com>
2024-07-06 23:28:34 -0400

    Added backend files: server side structure

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+dotenv.config({path:'config.env'});
+
+const port = process.env.PORT || 8000; 
+
 const app = express();
-const port = 3000;
+
+const connectDB = require('./server/database/connection');
+
+// Mongodb connection
+// connectDB();
+
+// Parser request to body-parser
+app.use(bodyparser.urlencoded({extended:true}));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set view engine
+app.set("view engine", "ejs");
+
+// app.use("/src", express.static(path.resolve(__dirname, "src")));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'src', 'login.html'));
 });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
-module.exports = app;
